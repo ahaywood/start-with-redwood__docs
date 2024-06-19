a5e8364ff8cdae216416daae3fb58f063f4d1391
Amy Haywood Dutton <me@amyhaywood.com>
2024-06-19 18:56:08 -0500

    🎉 Initial Commit

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [react(), mdx(), tailwind(), sitemap()],
+  output: "server",
+  adapter: vercel()
+});
